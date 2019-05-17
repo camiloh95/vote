cf4e15c984a7934862145ae35d01e5e4db824396
camiloias <camilo.hernandez@ias.com.co>
2019-05-17 07:48:35 -0500

    Created user login

---
+++ Vote.Common/Interfaces/IDialogService.cs
@@ -0,0 +1,7 @@
+﻿namespace Vote.Common.Interfaces
+{
+    public interface IDialogService
+    {
+        void Alert(string message, string title, string okbtnText);
+    }
+}
---
+++ Vote.Common/Services/IApiService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Vote.Common.Models;
+
+namespace Vote.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> ChangePasswordAsync(string urlBase, string servicePrefix, string controller, ChangePasswordRequest changePasswordRequest, string tokenType, string accessToken);
+
+        Task<Response> GetAlreadyVotedAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> GetCandidatesAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken);
+
+        Task<Response> GetTokenAsync(string urlBase, string servicePrefix, string controller, TokenRequest request);
+
+        Task<Response> GetUserByEmailAsync(string urlBase, string servicePrefix, string controller, string email, string tokenType, string accessToken);
+
+        Task<Response> GetVoteEventByIdAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> PostAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> PutAsync<T>(string urlBase, string servicePrefix, string controller, int id, T model, string tokenType, string accessToken);
+
+        Task<Response> PutAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> RecoverPasswordAsync(string urlBase, string servicePrefix, string controller, RecoverPasswordRequest recoverPasswordRequest);
+
+        Task<Response> RegisterUserAsync(string urlBase, string servicePrefix, string controller, NewUserRequest newUserRequest);
+    }
+}
