a85e257ac57d30e790f06fc78fe0f90779aa4d22
camiloias <camilo.hernandez@ias.com.co>
2019-05-24 11:39:25 -0500

    Register for new users

--- Vote.Common/Models/Stratum.cs
+++ Vote.Common/Models/Stratum.cs
@@ -9,5 +9,10 @@
 
         [JsonProperty("name")]
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
     }
 }
--- Vote.Common/ViewModels/RegisterViewModel.cs
+++ Vote.Common/ViewModels/RegisterViewModel.cs
@@ -16,14 +16,18 @@
         private readonly IDialogService dialogService;
         private List<Country> countries;
         private List<City> cities;
+        private List<Gender> genders;
+        private List<Stratum> stratums;
         private Country selectedCountry;
         private City selectedCity;
+        private Gender selectedGender;
+        private Stratum selectedStratum;
         private MvxCommand registerCommand;
         private string firstName;
         private string lastName;
         private string email;
-        private string address;
         private string phone;
+        private string occupation;
         private string password;
         private string confirmPassword;
 
@@ -65,12 +69,6 @@
             set => this.SetProperty(ref this.email, value);
         }
 
-        public string Address
-        {
-            get => this.address;
-            set => this.SetProperty(ref this.address, value);
-        }
-
         public string Phone
         {
             get => this.phone;
@@ -89,6 +87,12 @@
             set => this.SetProperty(ref this.confirmPassword, value);
         }
 
+        public string Occupation
+        {
+            get => this.occupation;
+            set => this.SetProperty(ref this.occupation, value);
+        }
+
         public List<Country> Countries
         {
             get => this.countries;
@@ -99,6 +103,18 @@
         {
             get => this.cities;
             set => this.SetProperty(ref this.cities, value);
+        }
+
+        public List<Gender> Genders
+        {
+            get => this.genders;
+            set => this.SetProperty(ref this.genders, value);
+        }
+
+        public List<Stratum> Stratums
+        {
+            get => this.stratums;
+            set => this.SetProperty(ref this.stratums, value);
         }
 
         public Country SelectedCountry
@@ -122,6 +138,26 @@
             }
         }
 
+        public Gender SelectedGender
+        {
+            get => selectedGender;
+            set
+            {
+                selectedGender = value;
+                RaisePropertyChanged(() => selectedGender);
+            }
+        }
+
+        public Stratum SelectedStratum
+        {
+            get => selectedStratum;
+            set
+            {
+                selectedStratum = value;
+                RaisePropertyChanged(() => selectedStratum);
+            }
+        }
+
         private async void LoadCountries()
         {
             var response = await this.apiService.GetListAsync<Country>(
@@ -140,14 +176,17 @@
 
         private async void RegisterUser()
         {
-            // TODO: Make the local validations
             var request = new NewUserRequest
             {
                 CityId = this.SelectedCity.Id,
                 Email = this.Email,
+                Occupation = this.Occupation,
+                Gender = this.SelectedGender.Id,
+                Stratum = this.SelectedStratum.Id,
                 FirstName = this.FirstName,
                 LastName = this.LastName,
-                Password = this.Password
+                Password = this.Password,
+                Phone = this.Phone
             };
 
             var response = await this.apiService.RegisterUserAsync(
@@ -156,11 +195,47 @@
                 "/Account",
                 request);
 
+            if (!response.IsSuccess)
+            {
+                this.dialogService.Alert("Error", "There where a problem trying to register the user", "Accept");
+                return;
+            }
+
             this.dialogService.Alert("Ok", "The user was created succesfully, you must " +
                 "confirm your user by the email sent to you and then you could login with " +
-                "the email and password entered.", "Accept");
+                "the email and password entered.", "Accept"); 
 
             await this.navigationService.Close(this);
+        }
+
+        public override void ViewAppeared()
+        {
+            base.ViewAppeared();
+            this.Genders = this.GetGenders();
+            this.Stratums = this.GetStratums();
+        }
+
+        public List<Stratum> GetStratums()
+        {
+            var list = new List<Stratum>();
+            list.Insert(0, new Stratum { Id = 0, Name = "1" });
+            list.Insert(1, new Stratum { Id = 1, Name = "2" });
+            list.Insert(2, new Stratum { Id = 2, Name = "3" });
+            list.Insert(3, new Stratum { Id = 3, Name = "4" });
+            list.Insert(4, new Stratum { Id = 4, Name = "5" });
+            list.Insert(5, new Stratum { Id = 5, Name = "6" });
+
+            return list;
+        }
+
+        public List<Gender> GetGenders()
+        {
+            var list = new List<Gender>();
+            list.Insert(0, new Gender { Id = 0, Name = "Male" });
+            list.Insert(1, new Gender { Id = 1, Name = "Female" });
+            list.Insert(2, new Gender { Id = 2, Name = "Other" });
+
+            return list;
         }
     }
 }
